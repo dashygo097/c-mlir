c5d354e82ffcd9dda65250cd2920ff5a1643d7c9
dashygo097 <dashygo097@github.com>
2026-02-05 00:06:03 +0800

    [tools.cmlirc] checkpoint

--- tools/cmlirc/Lib/ValueCategory.cpp
+++ tools/cmlirc/Lib/ValueCategory.cpp
@@ -1,11 +1,3 @@
-//===- ValueCategory.cc ------------------------------------------*- C++-*-===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #include "./ValueCategory.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
