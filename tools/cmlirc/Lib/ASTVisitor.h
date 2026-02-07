e9fbdf3a731dcc11124713b3341b89a7ace443d6
dashygo097 <dashygo097@gmail.com>
2026-02-07 22:21:30 +0800

    [tools.cmlirc] incremental operators; repetitive generation fixed

--- tools/cmlirc/Lib/ASTVisitor.h
+++ tools/cmlirc/Lib/ASTVisitor.h
@@ -14,9 +14,8 @@ public:
 
   bool TraverseFunctionDecl(clang::FunctionDecl *D);
   bool TraverseStmt(clang::Stmt *stmt);
-
-  bool VisitVarDecl(clang::VarDecl *decl);
-  bool VisitReturnStmt(clang::ReturnStmt *retStmt);
+  bool TraverseVarDecl(clang::VarDecl *decl);
+  bool TraverseReturnStmt(clang::ReturnStmt *retStmt);
 
   ContextManager &context_manager_;
 
@@ -40,6 +39,8 @@ private:
   mlir::Value generateDeclRefExpr(clang::DeclRefExpr *declRef, bool needLValue);
   mlir::Value generateArraySubscriptExpr(clang::ArraySubscriptExpr *expr,
                                          bool needLValue);
+  mlir::Value generateIncrementDecrement(clang::Expr *expr, bool isIncrement,
+                                         bool isPrefix);
 
   mlir::Value generateUnaryOperator(clang::UnaryOperator *unOp);
   mlir::Value generateBinaryOperator(clang::BinaryOperator *binOp);
