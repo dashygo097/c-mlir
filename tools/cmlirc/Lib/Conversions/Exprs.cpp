c5a77e5ce9adbb27156a2901cc0fbfcfef728a61
dashygo097 <dashygo097@github.com>
2026-02-10 15:10:40 +0800

    [cmlirc] polish for binaryExpr impl

--- tools/cmlirc/Lib/Conversions/Exprs.cpp
+++ tools/cmlirc/Lib/Conversions/Exprs.cpp
@@ -161,4 +161,45 @@ CMLIRCASTVisitor::generateArraySubscriptExpr(clang::ArraySubscriptExpr *expr,
   }
 }
 
+mlir::Value CMLIRCASTVisitor::generateCallExpr(clang::CallExpr *callExpr) {
+  mlir::OpBuilder &builder = context_manager_.Builder();
+
+  const clang::FunctionDecl *calleeDecl = callExpr->getDirectCallee();
+  if (!calleeDecl) {
+    llvm::errs() << "Indirect calls not supported yet\n";
+    return nullptr;
+  }
+
+  std::string calleeName = calleeDecl->getNameAsString();
+
+  llvm::SmallVector<mlir::Value, 4> argValues;
+  for (unsigned i = 0; i < callExpr->getNumArgs(); ++i) {
+    clang::Expr *argExpr = callExpr->getArg(i);
+    mlir::Value argValue = generateExpr(argExpr);
+    if (!argValue) {
+      llvm::errs() << "Failed to generate argument " << i << "\n";
+      return nullptr;
+    }
+    argValues.push_back(argValue);
+  }
+
+  clang::QualType returnType = calleeDecl->getReturnType();
+  mlir::Type mlirReturnType = convertType(builder, returnType);
+
+  llvm::SmallVector<mlir::Type, 1> returnTypes;
+  if (!mlir::isa<mlir::NoneType>(mlirReturnType)) {
+    returnTypes.push_back(mlirReturnType);
+  }
+
+  auto callOp = mlir::func::CallOp::create(
+      builder, builder.getUnknownLoc(), calleeName,
+      mlir::TypeRange{returnTypes}, mlir::ValueRange{argValues});
+
+  if (callOp.getNumResults() > 0) {
+    return callOp.getResult(0);
+  }
+
+  return nullptr;
+}
+
 } // namespace cmlirc
