f334212273edbe4f9738f909a6009591bec357f7
dashygo097 <dashygo097@gmail.com>
2026-02-12 16:15:15 +0800

    [tests] increment.c decrement.c

--- tests/cmlirc/decrement.c
+++ tests/cmlirc/decrement.c
@@ -1,13 +1,18 @@
-// RUN: cmlirc %s | FileCheck %s
+// RUN: split-file %s %t
 
+// RUN: cmlirc %t/pre_decrement.c | FileCheck %s --check-prefix=CHECKPRE
+// RUN: cmlirc %t/post_decrement.c --canonicalize | FileCheck %s --check-prefix=CHECKPOST
+
+//--- pre_decrement.c
 int pre_decrement(int a) { return --a; }
+
+// CHECKPRE: func.func @pre_decrement(%arg0: i32) -> i32
+// CHECKPRE-DAG: %[[c1_i32:.+]] = arith.constant 1 : i32
+// CHECKPRE: %[[V0:.*]] = arith.subi %arg0, %c1_i32 : i32
+// CHECKPRE: return %[[V0]] : i32
+
+//--- post_decrement.c
 int post_decrement(int a) { return a--; }
 
-// CHECK: func.func @pre_decrement(%arg0: i32) -> i32
-// CHECK: %c1_i32 = arith.constant 1 : i32
-// CHECK: %[[V0:.*]] = arith.subi %arg0, %c1_i32 : i32
-// CHECK: return %[[V0]] : i32
-// CHECK: func.func @post_decrement(%arg0: i32) -> i32
-// CHECK: %c1_i32 = arith.constant 1 : i32
-// CHECK: %[[V0:.*]] = arith.subi %arg0, %c1_i32 : i32
-// CHECK: return %arg0 : i32
+// CHECKPOST: func.func @post_decrement(%arg0: i32) -> i32
+// CHECKPOST: return %arg0 : i32
--- tests/cmlirc/increment.c
+++ tests/cmlirc/increment.c
@@ -1,13 +1,18 @@
-// RUN: cmlirc %s | FileCheck %s
+// RUN: split-file %s %t
 
+// RUN: cmlirc %t/pre_increment.c | FileCheck %s --check-prefix=CHECKPRE
+// RUN: cmlirc %t/post_increment.c --canonicalize | FileCheck %s --check-prefix=CHECKPOST
+
+//--- pre_increment.c
 int pre_increment(int a) { return ++a; }
+
+// CHECKPRE: func.func @pre_increment(%arg0: i32) -> i32
+// CHECKPRE-DAG: %[[c1_i32:.+]] = arith.constant 1 : i32
+// CHECKPRE: %[[V0:.*]] = arith.addi %arg0, %c1_i32 : i32
+// CHECKPRE: return %[[V0]] : i32
+
+//--- post_increment.c
 int post_increment(int a) { return a++; }
 
-// CHECK: func.func @pre_increment(%arg0: i32) -> i32
-// CHECK: %c1_i32 = arith.constant 1 : i32
-// CHECK: %[[V0:.*]] = arith.addi %arg0, %c1_i32 : i32
-// CHECK: return %[[V0]] : i32
-// CHECK: func.func @post_increment(%arg0: i32) -> i32
-// CHECK: %c1_i32 = arith.constant 1 : i32
-// CHECK: %[[V0:.*]] = arith.addi %arg0, %c1_i32 : i32
-// CHECK: return %arg0 : i32
+// CHECKPOST: func.func @post_increment(%arg0: i32) -> i32
+// CHECKPOST: return %arg0 : i32
