09d1b2fcd62f6cfac7ba738410b540a82e838f5e
dashygo097 <dashygo097@gmail.com>
2026-02-14 20:10:34 +0800

    [tests] mem2reg; [cmlic]: mem2reg pass impled

--- tests/cmlirc/mem2reg.c
+++ tests/cmlirc/mem2reg.c
@@ -1,4 +1,4 @@
-// RUN: cmlirc %s -function=dot -mem2reg | FileCheck %s
+// RUN: cmlirc %s -function=dot --mem2reg --symdce --cse --canonicalize --licm --sscp | FileCheck %s --check-prefix=CHECKDOT
 
 int dot(int *a, int *b, int n) {
   int result = 0;
@@ -8,16 +8,16 @@ int dot(int *a, int *b, int n) {
   return result;
 }
 
-// CHECK: func @dot(%arg0: memref<?xi32>, %arg1: memref<?xi32>, %arg2: i32) -> i32
-// CHECK-DAG: %c0 = arith.constant 0 : i32
-// CHECK-DAG: %c1 = arith.constant 1 : i32
-// CHECK-DAG: %c0_i32 = arith.constant 0 : i32
-// CHECK: %[[V0:.*]] = arith.index_cast %arg2 : i32 to index
-// CHECK: %[[V1:.*]] = scf.for %arg3 = %c0 to %[[V0]] step %c1 iter_args(%arg4 = %c0_i32) -> (i32) {
-// CHECK:   %[[V2:.*]] = memref.load %arg0[%arg3] : memref<?xi32>
-// CHECK:   %[[V3:.*]] = memref.load %arg1[%arg3] : memref<?xi32>
-// CHECK:   %[[V4:.*]] = arith.muli %[[V2]], %[[V3]] : i32
-// CHECK:   %[[V5:.*]] = arith.addi %[[V4]], %arg4 : i32
-// CHECK:   scf.yield %[[V5]] : i32
-// CHECK: }
-// CHECK: return %[[V1]] : i32
+// CHECKDOT: func @dot(%arg0: memref<?xi32>, %arg1: memref<?xi32>, %arg2: i32) -> i32
+// CHECKDOT-DAG: %[[c0:.+]] = arith.constant 0 : index
+// CHECKDOT-DAG: %[[c1:.+]] = arith.constant 1 : index
+// CHECKDOT-DAG: %[[c0_i32:.+]] = arith.constant 0 : i32
+// CHECKDOT: %[[V0:.*]] = arith.index_cast %arg2 : i32 to index
+// CHECKDOT: %[[V1:.*]] = scf.for %arg3 = %c0 to %[[V0]] step %c1 iter_args(%arg4 = %c0_i32) -> (i32) {
+// CHECKDOT:   %[[V2:.*]] = memref.load %arg0[%arg3] : memref<?xi32>
+// CHECKDOT:   %[[V3:.*]] = memref.load %arg1[%arg3] : memref<?xi32>
+// CHECKDOT:   %[[V4:.*]] = arith.muli %[[V2]], %[[V3]] : i32
+// CHECKDOT:   %[[V5:.*]] = arith.addi %arg4, %[[V4]] : i32
+// CHECKDOT:   scf.yield %[[V5]] : i32
+// CHECKDOT: }
+// CHECKDOT: return %[[V1]] : i32
