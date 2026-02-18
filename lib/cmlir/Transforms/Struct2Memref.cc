adb00c8d53fcf5bfccd52799d1e13f98827600f6
dashygo097 <dashygo097@github.com>
2026-02-19 00:07:36 +0800

    [cmlirc] struct2memref pass checkpoint

--- lib/cmlir/Transforms/Struct2Memref.cc
+++ lib/cmlir/Transforms/Struct2Memref.cc
@@ -1,5 +1,6 @@
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/IR/Dominance.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Support/LLVM.h"
@@ -38,98 +39,6 @@ static mlir::Type convertStructToMemref(mlir::LLVM::LLVMStructType structType,
  *  float y;
  *  } -> memref<2xf32>
  */
-
-struct ConvertFuncSignatureStruct2MemrefPattern
-    : public mlir::OpRewritePattern<mlir::func::FuncOp> {
-  using mlir::OpRewritePattern<mlir::func::FuncOp>::OpRewritePattern;
-
-  mlir::LogicalResult
-  matchAndRewrite(mlir::func::FuncOp funcOp,
-                  mlir::PatternRewriter &rewriter) const override {
-    auto funcType = funcOp.getFunctionType();
-    bool needConversion = false;
-
-    // check if any inputs have LLVM struct types
-    for (const mlir::Type &type : funcType.getInputs()) {
-      if (mlir::isa<mlir::LLVM::LLVMStructType>(type)) {
-        needConversion = true;
-        break;
-      }
-    }
-
-    // check if any results have LLVM struct types
-    if (!needConversion) {
-      for (const mlir::Type &type : funcType.getResults()) {
-        if (mlir::isa<mlir::LLVM::LLVMStructType>(type)) {
-          needConversion = true;
-          break;
-        }
-      }
-    }
-
-    if (!needConversion) {
-      return mlir::failure();
-    }
-
-    // convert input types
-    llvm::SmallVector<mlir::Type, 4> newInputTypes;
-    for (const mlir::Type &type : funcType.getInputs()) {
-      if (auto structType = mlir::dyn_cast<mlir::LLVM::LLVMStructType>(type)) {
-        auto memrefType =
-            convertStructToMemref(structType, funcOp.getContext());
-        if (!memrefType) {
-          return mlir::failure();
-        }
-        newInputTypes.push_back(memrefType);
-      } else {
-        newInputTypes.push_back(type);
-      }
-    }
-
-    // convert result types
-    for (const mlir::Type &type : funcType.getResults()) {
-      if (auto structType = mlir::dyn_cast<mlir::LLVM::LLVMStructType>(type)) {
-        auto memrefType =
-            convertStructToMemref(structType, funcOp.getContext());
-        if (!memrefType) {
-          return mlir::failure();
-        }
-        newInputTypes.push_back(memrefType);
-      } else {
-        newInputTypes.push_back(type);
-      }
-    }
-
-    auto newFuncType = mlir::FunctionType::get(
-        funcOp.getContext(), newInputTypes, funcType.getResults());
-
-    funcOp.setFunctionTypeAttr(mlir::TypeAttr::get(newFuncType));
-
-    mlir::Block &entryBlock = funcOp.getBody().front();
-    for (mlir::BlockArgument &arg : entryBlock.getArguments()) {
-      if (auto structType =
-              mlir::dyn_cast<mlir::LLVM::LLVMStructType>(arg.getType())) {
-        auto memrefType =
-            convertStructToMemref(structType, funcOp.getContext());
-        if (!memrefType) {
-          return mlir::failure();
-        }
-        arg.setType(memrefType);
-      }
-    }
-
-    return mlir::success();
-  }
-};
-
-struct ConvertStructAlloca2Memref
-    : public mlir::OpRewritePattern<mlir::LLVM::AllocaOp> {
-  using mlir::OpRewritePattern<mlir::LLVM::AllocaOp>::OpRewritePattern;
-
-  mlir::LogicalResult
-  matchAndRewrite(mlir::LLVM::AllocaOp allocaOp,
-                  mlir::PatternRewriter &rewriter) const override {}
-};
 
 struct Struct2MemrefPass
     : public mlir::PassWrapper<Struct2MemrefPass,
