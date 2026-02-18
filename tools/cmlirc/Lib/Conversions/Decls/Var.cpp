07fcefb0c4581f20d3c54b84a32d17c820b8713c
dashygo097 <dashygo097@gmail.com>
2026-02-18 17:01:46 +0800

    [cmlirc] struct features checkpoint

--- tools/cmlirc/Lib/Conversions/Decls/Var.cpp
+++ tools/cmlirc/Lib/Conversions/Decls/Var.cpp
@@ -24,9 +24,86 @@ bool CMLIRConverter::TraverseVarDecl(clang::VarDecl *decl) {
       SM.getSpellingLineNumber(loc), SM.getSpellingColumnNumber(loc));
 
   mlir::OpBuilder &builder = context_manager_.Builder();
-
   clang::QualType clangType = decl->getType();
   mlir::Type mlirType = convertType(clangType);
+
+  if (!mlirType) {
+    llvm::errs() << "Failed to convert type for variable: "
+                 << decl->getNameAsString() << "\n";
+    return false;
+  }
+
+  if (clangType->isStructureType()) {
+    auto structType = mlir::dyn_cast<mlir::LLVM::LLVMStructType>(mlirType);
+    if (!structType) {
+      llvm::errs() << "Expected struct type\n";
+      return false;
+    }
+
+    auto ptrType = mlir::LLVM::LLVMPointerType::get(builder.getContext());
+    auto one = mlir::LLVM::ConstantOp::create(
+        builder, mlirLoc, builder.getI64Type(), builder.getI64IntegerAttr(1));
+
+    auto allocaOp = mlir::LLVM::AllocaOp::create(builder, mlirLoc, ptrType,
+                                                 structType, one, 0);
+
+    symbolTable[decl] = allocaOp.getResult();
+
+    if (decl->hasInit()) {
+      clang::Expr *init = decl->getInit();
+
+      if (auto *initList = llvm::dyn_cast<clang::InitListExpr>(init)) {
+        const clang::RecordType *recordType = clangType->getAsStructureType();
+        if (!recordType) {
+          llvm::errs() << "Expected record type\n";
+          return false;
+        }
+
+        const clang::RecordDecl *recordDecl = recordType->getDecl();
+        unsigned fieldIndex = 0;
+
+        for (auto *_ : recordDecl->fields()) {
+          if (fieldIndex >= initList->getNumInits())
+            break;
+
+          clang::Expr *initExpr = initList->getInit(fieldIndex);
+          mlir::Value initValue = generateExpr(initExpr);
+
+          if (!initValue) {
+            llvm::errs() << "Failed to generate init value for field\n";
+            fieldIndex++;
+            continue;
+          }
+
+          auto zero = mlir::LLVM::ConstantOp::create(
+              builder, mlirLoc, builder.getI32Type(),
+              builder.getI32IntegerAttr(0));
+          auto fieldIdx = mlir::LLVM::ConstantOp::create(
+              builder, mlirLoc, builder.getI32Type(),
+              builder.getI32IntegerAttr(fieldIndex));
+
+          llvm::SmallVector<mlir::Value, 2> indices;
+          indices.push_back(zero);
+          indices.push_back(fieldIdx);
+
+          auto fieldPtr =
+              mlir::LLVM::GEPOp::create(builder, mlirLoc, ptrType, structType,
+                                        allocaOp.getResult(), indices);
+
+          mlir::LLVM::StoreOp::create(builder, mlirLoc, initValue, fieldPtr);
+
+          fieldIndex++;
+        }
+      } else {
+        mlir::Value initValue = generateExpr(init);
+        if (initValue) {
+          mlir::LLVM::StoreOp::create(builder, mlirLoc, initValue, allocaOp);
+        }
+      }
+    }
+
+    return true;
+  }
 
   mlir::Type allocaType;
   if (auto memrefType = mlir::dyn_cast<mlir::MemRefType>(mlirType)) {
@@ -36,7 +113,7 @@ bool CMLIRConverter::TraverseVarDecl(clang::VarDecl *decl) {
   }
 
   auto allocaOp = mlir::memref::AllocaOp::create(
-      builder, mlirLoc, mlir::dyn_cast<mlir::MemRefType>(allocaType));
+      builder, mlirLoc, mlir::cast<mlir::MemRefType>(allocaType));
 
   symbolTable[decl] = allocaOp.getResult();
 
@@ -54,63 +131,7 @@ bool CMLIRConverter::TraverseVarDecl(clang::VarDecl *decl) {
     }
   }
 
-  if (clangType->isStructureType()) {
-    auto structType = mlir::dyn_cast<mlir::LLVM::LLVMStructType>(mlirType);
-    if (!structType) {
-      llvm::errs() << "Expected struct type\n";
-      return false;
-    }
-
-    if (decl->hasInit()) {
-      clang::Expr *init = decl->getInit();
-
-      if (auto *initList = llvm::dyn_cast<clang::InitListExpr>(init)) {
-        const clang::RecordType *recordType = clangType->getAsStructureType();
-        if (!recordType) {
-          llvm::errs() << "Expected record type\n";
-          return false;
-        }
-
-        const clang::RecordDecl *recordDecl = recordType->getDecl();
-        unsigned fieldIndex = 0;
-        for (auto *_ : recordDecl->fields()) {
-          if (fieldIndex >= initList->getNumInits())
-            break;
-
-          clang::Expr *initExpr = initList->getInit(fieldIndex);
-          mlir::Value initValue = generateExpr(initExpr);
-
-          if (!initValue) {
-            llvm::errs() << "Failed to generate init value for field\n";
-            fieldIndex++;
-            continue;
-          }
-
-          mlir::Value loadedStruct = mlir::memref::LoadOp::create(
-              builder, mlirLoc, allocaOp.getResult());
-
-          mlir::Value updatedStruct = mlir::LLVM::InsertValueOp::create(
-              builder, mlirLoc, loadedStruct, initValue, fieldIndex);
-
-          mlir::memref::StoreOp::create(builder, mlirLoc, updatedStruct,
-                                        allocaOp.getResult());
-
-          fieldIndex++;
-        }
-      } else {
-        mlir::Value initValue = generateExpr(init);
-        if (initValue) {
-          mlir::memref::StoreOp::create(builder, mlirLoc, initValue,
-                                        allocaOp.getResult(),
-                                        mlir::ValueRange{});
-        }
-      }
-    }
-
-    return true;
-  }
-
-  return RecursiveASTVisitor::TraverseVarDecl(decl);
+  return true;
 }
 
 } // namespace cmlirc
---
+++ tools/cmlirc/Lib/Conversions/Exprs/CXXConstruct.cpp
@@ -0,0 +1,100 @@
+#include "../../Converter.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+
+namespace cmlirc {
+
+mlir::Value CMLIRConverter::generateCXXConstructExpr(
+    clang::CXXConstructExpr *constructExpr) {
+  mlir::OpBuilder &builder = context_manager_.Builder();
+  mlir::Location loc = builder.getUnknownLoc();
+
+  clang::QualType type = constructExpr->getType();
+
+  if (type->isStructureOrClassType()) {
+    mlir::Type structType = convertType(type);
+    auto llvmStructType =
+        mlir::dyn_cast<mlir::LLVM::LLVMStructType>(structType);
+
+    if (!llvmStructType) {
+      llvm::errs() << "Expected LLVM struct type for constructor\n";
+      return nullptr;
+    }
+
+    if (constructExpr->getNumArgs() == 0) {
+      auto undefValue =
+          mlir::LLVM::UndefOp::create(builder, loc, llvmStructType);
+
+      mlir::Value result = undefValue.getResult();
+      unsigned numFields = llvmStructType.getBody().size();
+
+      for (unsigned i = 0; i < numFields; ++i) {
+        mlir::Type fieldType = llvmStructType.getBody()[i];
+        mlir::Value zeroValue;
+
+        if (auto intType = mlir::dyn_cast<mlir::IntegerType>(fieldType)) {
+          zeroValue =
+              mlir::arith::ConstantOp::create(
+                  builder, loc, intType, builder.getIntegerAttr(intType, 0))
+                  .getResult();
+        } else if (auto floatType =
+                       mlir::dyn_cast<mlir::FloatType>(fieldType)) {
+          zeroValue =
+              mlir::arith::ConstantOp::create(
+                  builder, loc, floatType, builder.getFloatAttr(floatType, 0.0))
+                  .getResult();
+        } else {
+          // 其他类型：保持 undef
+          continue;
+        }
+
+        result = mlir::LLVM::InsertValueOp::create(builder, loc, result,
+                                                   zeroValue, i)
+                     .getResult();
+      }
+
+      return result;
+    }
+
+    mlir::Value result =
+        mlir::LLVM::UndefOp::create(builder, loc, llvmStructType).getResult();
+
+    for (unsigned i = 0; i < constructExpr->getNumArgs(); ++i) {
+      clang::Expr *arg = constructExpr->getArg(i);
+      mlir::Value argValue = generateExpr(arg);
+
+      if (!argValue) {
+        llvm::errs() << "Failed to generate constructor argument " << i << "\n";
+        return nullptr;
+      }
+
+      result =
+          mlir::LLVM::InsertValueOp::create(builder, loc, result, argValue, i)
+              .getResult();
+    }
+
+    return result;
+  }
+
+  if (constructExpr->getNumArgs() == 1) {
+    return generateExpr(constructExpr->getArg(0));
+  }
+
+  if (constructExpr->getNumArgs() == 0) {
+    mlir::Type mlirType = convertType(type);
+
+    if (auto intType = mlir::dyn_cast<mlir::IntegerType>(mlirType)) {
+      return mlir::arith::ConstantOp::create(builder, loc, intType,
+                                             builder.getIntegerAttr(intType, 0))
+          .getResult();
+    } else if (auto floatType = mlir::dyn_cast<mlir::FloatType>(mlirType)) {
+      return mlir::arith::ConstantOp::create(
+                 builder, loc, floatType, builder.getFloatAttr(floatType, 0.0))
+          .getResult();
+    }
+  }
+
+  llvm::errs() << "Unsupported constructor expression\n";
+  return nullptr;
+}
+
+} // namespace cmlirc
