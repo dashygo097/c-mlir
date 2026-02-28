54e3242792d802dae9467a64723457c07e07c928
dashygo097 <dashygo097@github.com>
2026-02-28 19:50:50 +0800

    [cmlirc] do-while

--- tools/cmlirc/Lib/Conversions/Stmts/CF/Do.cpp
+++ tools/cmlirc/Lib/Conversions/Stmts/CF/Do.cpp
@@ -1,5 +1,41 @@
 #include "../../../Converter.h"
-#include "./CFUtils.h"
+#include "../../Utils/Casts.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 
-namespace cmlirc {} // namespace cmlirc
+namespace cmlirc {
+
+bool CMLIRConverter::TraverseDoStmt(clang::DoStmt *doStmt) {
+  if (!currentFunc)
+    return true;
+
+  mlir::OpBuilder &builder = context_manager_.Builder();
+  mlir::Location loc = builder.getUnknownLoc();
+
+  auto whileOp = mlir::scf::WhileOp::create(
+      builder, loc, mlir::TypeRange{}, mlir::ValueRange{},
+      [&](mlir::OpBuilder &b, mlir::Location l, mlir::ValueRange args) {
+        mlir::scf::ConditionOp::create(b, l, detail::boolConst(b, l, true),
+                                       mlir::ValueRange{});
+      },
+      [&](mlir::OpBuilder &b, mlir::Location l, mlir::ValueRange args) {
+        mlir::scf::YieldOp::create(b, l, mlir::ValueRange{});
+      });
+
+  mlir::Block *beforeBlock = &whileOp.getBefore().front();
+  beforeBlock->back().erase();
+
+  builder.setInsertionPointToEnd(beforeBlock);
+  loopStack_.push_back({beforeBlock, &whileOp.getAfter().front()});
+  TraverseStmt(doStmt->getBody());
+  loopStack_.pop_back();
+
+  mlir::Value cond = doStmt->getCond()
+                         ? convertToBool(generateExpr(doStmt->getCond()))
+                         : detail::boolConst(builder, loc, true);
+  mlir::scf::ConditionOp::create(builder, loc, cond, mlir::ValueRange{});
+
+  builder.setInsertionPointAfter(whileOp);
+  return true;
+}
+
+} // namespace cmlirc
