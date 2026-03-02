b2b7384fc45bdf60900d2268a22b4e05714b88cb
dashygo097 <dashygo097@github.com>
2026-03-03 00:55:34 +0800

    [tests] max4.c

--- tests/cmlirc/max.c
+++ tests/cmlirc/max.c
@@ -1,6 +1,7 @@
 // RUN: split-file %s %t
 // RUN: cmlirc %t/max2.c | FileCheck %s --check-prefix=CHECK2
 // RUN: cmlirc %t/max3.c | FileCheck %s --check-prefix=CHECK3
+// RUN: cmlirc %t/max4.c | FileCheck %s --check-prefix=CHECK4
 
 //--- max2.c
 int max2(int a, int b) {
@@ -34,3 +35,24 @@ int max3(int a, int b, int c) {
 // CHECK3: ^bb2:
 // CHECK3: %[[V4:.*]] = arith.cmpi sgt, %arg1, %arg2 : i32
 // CHECK3: cf.cond_br %[[V4]], ^bb1(%arg1 : i32), ^bb1(%arg2 : i32)
+
+//--- max4.c
+int max4(int a, int b, int c, int d) {
+  int max = a;
+  if (b > max)
+    max = b;
+  if (c > max)
+    max = c;
+  if (d > max)
+    return d;
+  return max;
+}
+
+// CHECK4: func @max4(%arg0: i32, %arg1: i32, %arg2: i32, %arg3: i32) -> i32
+// CHECK4: %[[V0:.*]] = arith.cmpi sgt, %arg1, %arg0 : i32
+// CHECK4: %[[V1:.*]] = arith.select %[[V0]], %arg1, %arg0 : i32
+// CHECK4: %[[V2:.*]] = arith.cmpi sgt, %arg2, %[[V1]] : i32
+// CHECK4: %[[V3:.*]] = arith.select %[[V2]], %arg2, %[[V1]] : i32
+// CHECK4: %[[V4:.*]] = arith.cmpi sgt, %arg3, %[[V3]] : i32
+// CHECK4: %[[V5:.*]] = arith.select %[[V4]], %arg3, %[[V3]] : i32
+// CHECK4: return %[[V5]] : i32
