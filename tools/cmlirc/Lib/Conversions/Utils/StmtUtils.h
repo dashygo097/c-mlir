84e27fca313d48c57ed351b258ef6381ee2114fd
dashygo097 <dashygo097@gmail.com>
2026-03-04 12:29:18 +0800

    [cmlirc] checkpoint

--- tools/cmlirc/Lib/Conversions/Utils/StmtUtils.h
+++ tools/cmlirc/Lib/Conversions/Utils/StmtUtils.h
@@ -77,23 +77,93 @@ inline bool stmtHasReturnInSwitch(const clang::Stmt *stmt) {
   return false;
 }
 
-inline bool stmtHasReturnInCompound(const clang::Stmt *stmt) {
+inline bool stmtHasReturnInAnyControlFlow(const clang::Stmt *stmt) {
+  return stmtHasReturnInIf(stmt) || stmtHasReturnInLoop(stmt) ||
+         stmtHasReturnInSwitch(stmt);
+}
+
+inline bool stmtHasBreak(const clang::Stmt *stmt) {
   if (!stmt) {
     return false;
   }
   for (const auto *child : stmt->children()) {
-    if (mlir::isa<clang::CompoundStmt>(child)) {
-      if (stmtHasReturnRecursively(child)) {
-        return true;
-      }
+    if (mlir::isa<clang::BreakStmt>(child)) {
+      return true;
     }
   }
   return false;
 }
 
-inline bool stmtHasReturnInAnyControlFlow(const clang::Stmt *stmt) {
-  return stmtHasReturnInIf(stmt) || stmtHasReturnInLoop(stmt) ||
-         stmtHasReturnInSwitch(stmt) || stmtHasReturnInCompound(stmt);
+inline bool stmtHasBreakRecursively(const clang::Stmt *stmt) {
+  if (!stmt) {
+    return false;
+  }
+  if (mlir::isa<clang::BreakStmt>(stmt)) {
+    return true;
+  }
+  for (const auto *child : stmt->children()) {
+    if (stmtHasBreakRecursively(child)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+inline bool stmtHasBreakInLoop(const clang::Stmt *stmt) {
+  if (!stmt) {
+    return false;
+  }
+  for (const auto *child : stmt->children()) {
+    if ((mlir::isa<clang::ForStmt>(child) ||
+         mlir::isa<clang::WhileStmt>(child) ||
+         mlir::isa<clang::DoStmt>(child)) &&
+        stmtHasBreakRecursively(child)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+inline bool stmtHasContinue(const clang::Stmt *stmt) {
+  if (!stmt) {
+    return false;
+  }
+  for (const auto *child : stmt->children()) {
+    if (mlir::isa<clang::ContinueStmt>(child)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+inline bool stmtHasContinueRecursively(const clang::Stmt *stmt) {
+  if (!stmt) {
+    return false;
+  }
+  if (mlir::isa<clang::ContinueStmt>(stmt)) {
+    return true;
+  }
+  for (const auto *child : stmt->children()) {
+    if (stmtHasContinueRecursively(child)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+inline bool stmtHasContinueInLoop(const clang::Stmt *stmt) {
+  if (!stmt) {
+    return false;
+  }
+  for (const auto *child : stmt->children()) {
+    if ((mlir::isa<clang::ForStmt>(child) ||
+         mlir::isa<clang::WhileStmt>(child) ||
+         mlir::isa<clang::DoStmt>(child)) &&
+        stmtHasContinueRecursively(child)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 } // namespace cmlirc::detail
