321d03e1f80732a38cc7a729348c32c692386470
dashygo097 <dashygo097@github.com>
2026-03-04 15:18:42 +0800

    [cmlirc] test cases

--- tools/cmlirc/Lib/Conversions/Utils/Numerics.h
+++ tools/cmlirc/Lib/Conversions/Utils/Numerics.h
@@ -5,11 +5,13 @@
 #include "./Operators.h"
 
 namespace cmlirc::detail {
-
 // Arithmetic
 inline mlir::Value addi(mlir::OpBuilder &builder, mlir::Location loc,
                         mlir::Value value, int64_t amount) {
-  mlir::Value amountValue = intConst(builder, loc, value.getType(), amount);
+  mlir::Value amountValue =
+      mlir::isa<mlir::IndexType>(value.getType())
+          ? indexConst(builder, loc, amount)
+          : intConst(builder, loc, value.getType(), amount);
   return emitIntOp<mlir::arith::AddIOp>(builder, loc, value, amountValue);
 }
 
@@ -19,9 +21,22 @@ inline mlir::Value addf(mlir::OpBuilder &builder, mlir::Location loc,
   return emitFloatOp<mlir::arith::AddFOp>(builder, loc, value, amountValue);
 }
 
+inline mlir::Value add(mlir::OpBuilder &builder, mlir::Location loc,
+                       mlir::Value lhs, mlir::Value rhs) {
+  if (mlir::isa<mlir::IntegerType>(lhs.getType()) ||
+      mlir::isa<mlir::IndexType>(lhs.getType()))
+    return emitIntOp<mlir::arith::AddIOp>(builder, loc, lhs, rhs);
+  if (mlir::isa<mlir::FloatType>(lhs.getType()))
+    return emitFloatOp<mlir::arith::AddFOp>(builder, loc, lhs, rhs);
+  return nullptr;
+}
+
 inline mlir::Value subi(mlir::OpBuilder &builder, mlir::Location loc,
                         mlir::Value value, int64_t amount) {
-  mlir::Value amountValue = intConst(builder, loc, value.getType(), amount);
+  mlir::Value amountValue =
+      mlir::isa<mlir::IndexType>(value.getType())
+          ? indexConst(builder, loc, amount)
+          : intConst(builder, loc, value.getType(), amount);
   return emitIntOp<mlir::arith::SubIOp>(builder, loc, value, amountValue);
 }
 
@@ -31,15 +46,37 @@ inline mlir::Value subf(mlir::OpBuilder &builder, mlir::Location loc,
   return emitFloatOp<mlir::arith::SubFOp>(builder, loc, value, amountValue);
 }
 
+inline mlir::Value sub(mlir::OpBuilder &builder, mlir::Location loc,
+                       mlir::Value lhs, mlir::Value rhs) {
+  if (mlir::isa<mlir::IntegerType>(lhs.getType()) ||
+      mlir::isa<mlir::IndexType>(lhs.getType()))
+    return emitIntOp<mlir::arith::SubIOp>(builder, loc, lhs, rhs);
+  if (mlir::isa<mlir::FloatType>(lhs.getType()))
+    return emitFloatOp<mlir::arith::SubFOp>(builder, loc, lhs, rhs);
+  return nullptr;
+}
+
 inline mlir::Value negi(mlir::OpBuilder &builder, mlir::Location loc,
                         mlir::Value value) {
-  mlir::Value zero = intConst(builder, loc, value.getType(), 0);
+  mlir::Value zero = mlir::isa<mlir::IndexType>(value.getType())
+                         ? indexConst(builder, loc, 0)
+                         : intConst(builder, loc, value.getType(), 0);
   return emitIntOp<mlir::arith::SubIOp>(builder, loc, zero, value);
 }
 
 inline mlir::Value negf(mlir::OpBuilder &builder, mlir::Location loc,
                         mlir::Value value) {
   return emitFloatOp<mlir::arith::NegFOp>(builder, loc, value);
+}
+
+inline mlir::Value neg(mlir::OpBuilder &builder, mlir::Location loc,
+                       mlir::Value value) {
+  if (mlir::isa<mlir::IntegerType>(value.getType()) ||
+      mlir::isa<mlir::IndexType>(value.getType()))
+    return negi(builder, loc, value);
+  if (mlir::isa<mlir::FloatType>(value.getType()))
+    return negf(builder, loc, value);
+  return nullptr;
 }
 
 // Bitwise
@@ -49,24 +86,17 @@ inline mlir::Value noti(mlir::OpBuilder &builder, mlir::Location loc,
   return emitIntOp<mlir::arith::XOrIOp>(builder, loc, value, allOnes);
 }
 
-// Logical
-inline mlir::Value notl(mlir::OpBuilder &builder, mlir::Location loc,
-                        mlir::Value value) {
-  return emitIntOp<mlir::arith::XOrIOp>(builder, loc, value,
-                                        boolConst(builder, loc, true));
-}
-
-inline mlir::Value andl(mlir::OpBuilder &builder, mlir::Location loc,
+inline mlir::Value andi(mlir::OpBuilder &builder, mlir::Location loc,
                         mlir::Value lhs, mlir::Value rhs) {
   return emitIntOp<mlir::arith::AndIOp>(builder, loc, lhs, rhs);
 }
 
-inline mlir::Value orl(mlir::OpBuilder &builder, mlir::Location loc,
+inline mlir::Value ori(mlir::OpBuilder &builder, mlir::Location loc,
                        mlir::Value lhs, mlir::Value rhs) {
   return emitIntOp<mlir::arith::OrIOp>(builder, loc, lhs, rhs);
 }
 
-inline mlir::Value xorl(mlir::OpBuilder &builder, mlir::Location loc,
+inline mlir::Value xori(mlir::OpBuilder &builder, mlir::Location loc,
                         mlir::Value lhs, mlir::Value rhs) {
   return emitIntOp<mlir::arith::XOrIOp>(builder, loc, lhs, rhs);
 }
