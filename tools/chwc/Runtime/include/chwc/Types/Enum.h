364f2e541903a5050d8ac11d070563a1f00b8daf
dashygo097 <dashygo097@gmail.com>
2026-05-09 22:02:13 +0800

    [chwc] Runtime Lib: fix Enum

--- tools/chwc/Runtime/include/chwc/Types/Enum.h
+++ tools/chwc/Runtime/include/chwc/Types/Enum.h
@@ -2,14 +2,25 @@
 #define CHWC_RUNTIME_TYPES_ENUM_H
 
 #include "chwc/Types/UInt.h"
-#include <cmath>
 
 namespace chwc {
 
-using Bool = UInt<1>;
+namespace impl {
+constexpr auto enumBits(std::size_t number) -> std::size_t {
+  std::size_t bits = 0;
+  std::size_t values = 1;
 
-template <std::size_t Number>
-using Enum = UInt<static_cast<int>(ceil(log2(Number)))>;
+  while (values < number) {
+    values <<= 1;
+    ++bits;
+  }
+
+  return bits == 0 ? 1 : bits;
+}
+} // namespace impl
+
+template <std::size_t Number> using Enum = UInt<impl::enumBits(Number)>;
+using Bool = UInt<1>;
 
 } // namespace chwc
 
